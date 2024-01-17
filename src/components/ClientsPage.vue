6bf14aa4ed5a9a470c6852f0dcacf414005481c2
Jared Hatfield <jjhatf02@gmail.com>
2024-01-16 22:58:29 -0500

    Clients list supports pagination

--- src/components/ClientsPage.vue
+++ src/components/ClientsPage.vue
@@ -1,67 +1,106 @@
 <template>
-    <div class="container mt-4">
-        <h2>Clients</h2>
-        <table class="table table-striped table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th scope="col">Client Id</th>
-                    <th scope="col">Description</th>
-                    <th scope="col" class="text-end">
-                      <router-link :to="'/client'" class="btn btn-success">
-                        <i class="bi bi-plus"></i>
-                      </router-link>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="client in clients" :key="client.clientId">
-                    <th scope="row">{{ client.clientId }}</th>
-                    <td>{{ client.description }}</td>
-                    <td class="text-end">
-                      <router-link :to="'/clients/' + client.clientId" class="btn btn-primary">
-                        <i class="bi bi-arrow-right"></i>
-                      </router-link>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+  <div class="container mt-4">
+      <h2>Clients</h2>
+      <table class="table table-striped table-hover">
+          <thead class="table-dark">
+              <tr>
+                  <th scope="col">Client Id</th>
+                  <th scope="col">Description</th>
+                  <th scope="col" class="text-end">
+                    <router-link :to="'/client'" class="btn btn-success">
+                      <i class="bi bi-plus"></i>
+                    </router-link>
+                  </th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="edge in clients.edges" :key="edge.node.clientId">
+                  <th scope="row">{{ edge.node.clientId }}</th>
+                  <td>{{ edge.node.description }}</td>
+                  <td class="text-end">
+                    <router-link :to="'/clients/' + edge.node.clientId" class="btn btn-primary">
+                      <i class="bi bi-arrow-right"></i>
+                    </router-link>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+      <div class="d-flex justify-content-center mb-3">
+          <div class="btn-group pagination-controls" role="group" aria-label="Pagination Controls">
+              <button @click="loadPrevious()" :disabled="!clients.pageInfo.hasPreviousPage" class="btn btn-primary">
+                  <i class="bi bi-chevron-left"></i> Previous
+              </button>
+              <button @click="loadNext()" :disabled="!clients.pageInfo.hasNextPage" class="btn btn-primary">
+                  Next <i class="bi bi-chevron-right"></i>
+              </button>
+          </div>
+      </div>
     </div>
 </template>
-
 
 <script>
 import { gql } from '@apollo/client/core';
 import client from '../apollo-client';
 
 const GET_CLIENTS = gql`
-  query Clients {
-    clients {
-      clientId
-      description
+  query Clients($first: Int, $after: String, $last: Int, $before: String) {
+    clients(first: $first, after: $after, last: $last, before: $before) {
+      edges {
+        cursor
+        node {
+          clientId
+          description
+        }
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
     }
   }
 `;
+
+const PERPAGE = 10;
 
 export default {
   name: 'ClientsPage',
   data() {
     return {
-      clients: [],
+      clients: {
+        edges: [],
+        pageInfo: {
+          hasNextPage: false,
+          hasPreviousPage: false,
+          startCursor: null,
+          endCursor: null,
+        },
+      },
     };
   },
   mounted() {
-    this.loadClients();
+    this.loadClients({ first: PERPAGE });
   },
   methods: {
-    async loadClients() {
-
-      
+    async loadClients({ first, after, last, before }) {
       try {
-        const { data } = await client.query({ query: GET_CLIENTS });
+        const { data } = await client.query({
+          query: GET_CLIENTS,
+          variables: { first, after, last, before },
+        });
         this.clients = data.clients;
       } catch (error) {
         console.error("Error fetching clients:", error);
       }
+    },
+    loadNext() {
+      const { endCursor } = this.clients.pageInfo;
+      this.loadClients({ first: PERPAGE, after: endCursor });
+    },
+    loadPrevious() {
+      const { startCursor } = this.clients.pageInfo;
+      this.loadClients({ last: PERPAGE, before: startCursor });
     },
   },
 };
