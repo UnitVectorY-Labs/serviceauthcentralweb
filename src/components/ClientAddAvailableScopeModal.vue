bbc0e1f5d1d821f253646377ae7010e0896bb2fa
Jared Hatfield <jjhatf02@gmail.com>
2024-02-06 15:46:27 -0500

    Restructure prep for adding available scopes

---
+++ src/components/ClientAddAvailableScopeModal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modal fade" id="clientAddAvailableScopeModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="clientAddAvailableScopeModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="clientAddAvailableScopeModalLabel">Add Available Scope</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        TODO IMPLEMENT
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import { gql } from '@apollo/client/core';
+// import client from '../apollo-client';
+
+
+
+export default {
+    props: {
+        client: Object
+    },
+    data() {
+        return {
+
+            loading: false,
+            errorMessage: null,
+            authorizedSuccess: false
+        };
+    },
+    methods: {
+    }
+};
+</script>
--- src/components/ClientDetailsPage.vue
+++ src/components/ClientDetailsPage.vue
@@ -15,10 +15,11 @@
         <ClientDeauthorizeModal ref="deauthorizeModal" v-if="client" :client="client" @refreshClient="refreshClient" />
         <ClientAddJwtBearerModal ref="addJwtBearerModal" v-if="client" :client="client" @refreshClient="refreshClient" />
         <clientDeauthorizeJwtBearerModal ref="deauthorizeJwtBearerModal" v-if="client" :client="client" @refreshClient="refreshClient" />
-
+        <ClientAddAvailableScopeModal ref="addAvailableScopeModal" v-if="client" :client="client" />
         <div class="row">
           <div class="col-lg-6 col-sm-12">
             <h3>Client</h3>
+            <span class="text-muted">A client is used by a service for both making requests to other clients and for receiving requests from other clients.</span>
             <table class="table table-striped table-hover">
               <thead class="table-dark">
                   <tr>
@@ -47,97 +48,145 @@
                       <td><DateTimeComponent :date="client.clientCreated" /></td>
                       <td></td>
                   </tr>
-                  <tr v-if="client.managementPermissions.canAddClientSecret || client.managementPermissions.canDeleteClientSecret">
-                      <td>Client Secret 1</td>
-                      <td>
-                        <span :class="{'me-2': true, 'badge': true, 'bg-primary': client.clientSecret1Set, 'bg-secondary': !client.clientSecret1Set}">
-                          {{ client.clientSecret1Set ? 'Set' : 'Not Set' }}
-                        </span>
-                        <DateTimeComponent v-if="client.clientSecret1Set" :date="client.clientSecret1Updated" />
-                      </td>
-                      <td>
-                          <div v-if="client.clientSecret1Set">
-                              <button v-if="client.managementPermissions.canDeleteClientSecret" type="button" class="btn btn-danger float-end" @click="openDeleteSecretModal('secret1')" data-bs-toggle="modal" data-bs-target="#deleteClientSecretModal" title="Delete Secret 1">
-                                  <i class="bi bi-trash"></i>
-                              </button>
-                          </div>
-                          <div v-else>
-                              <button v-if="client.managementPermissions.canAddClientSecret" type="button" class="btn btn-success float-end" @click="openSecretModal('secret1')" data-bs-toggle="modal" data-bs-target="#clientCreateSecretModal" title="Create Secret 1">
-                                  <i class="bi bi-plus"></i>
-                              </button>
-                          </div>
-                      </td>
-                  </tr>
-                  <tr v-if="client.managementPermissions.canAddClientSecret || client.managementPermissions.canDeleteClientSecret">
-                      <td>Client Secret 2</td>
-                      <td>
-                        <span :class="{'me-2': true, 'badge': true, 'bg-primary': client.clientSecret2Set, 'bg-secondary': !client.clientSecret2Set}">
-                          {{ client.clientSecret2Set ? 'Set' : 'Not Set' }}
-                        </span>
-                        <DateTimeComponent v-if="client.clientSecret2Set" :date="client.clientSecret2Updated" />
-                      </td>
-                      <td>
-                          <div v-if="client.clientSecret2Set">
-                              <button v-if="client.managementPermissions.canDeleteClientSecret" type="button" class="btn btn-danger float-end" @click="openDeleteSecretModal('secret2')" data-bs-toggle="modal" data-bs-target="#deleteClientSecretModal" title="Delete Secret 2">
-                                  <i class="bi bi-trash"></i>
-                              </button>
-                          </div>
-                          <div v-else>
-                              <button v-if="client.managementPermissions.canAddClientSecret" type="button" class="btn btn-success float-end" @click="openSecretModal('secret2')" data-bs-toggle="modal" data-bs-target="#clientCreateSecretModal" title="Create Secret 2">
-                                  <i class="bi bi-plus"></i>
-                              </button>
-                          </div>
-                      </td>
-                  </tr>
               </tbody>
           </table>
 
+          <h3>Client Available Scopes</h3>
+          <span class="text-muted">The list of scopes this client recognizes that can be requested by another client if authorization is granted.</span>
+          <table class="table table-striped table-hover">
+            <thead class="table-dark">
+                  <tr>
+                      <th scope="col">Scope</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">
+                          <button type="button" class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#clientAddAvailableScopeModal" title="Add Available Scope">
+                              <i class="bi bi-plus"></i>
+                          </button>
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                <tr v-for="availableScope in client.availableScopes" :key="availableScope.scope">
+                    <td><span class="badge text-bg-dark">{{ availableScope.scope }}</span></td>
+                    <td>{{ availableScope.description }}</td>
+                    <td></td>
+                  </tr>
+              </tbody>
+          </table>
+          <div v-if="client.availableScopes.length == 0" class="text-center p-3">
+              <span class="fw-lighter">No client available scopes.</span>
+            </div>
+
           </div>
 
-          <div class="col-lg-6 col-sm-12"  v-if="client.managementPermissions.canAddClientAuthorization || client.managementPermissions.canDeleteClientAuthorization">
-            <h3>Client Authorizations</h3>
-            <div v-for="bearer in client.jwtBearer" :key="bearer.id">
+          <div class="col-lg-6 col-sm-12">
+            <div v-if="client.managementPermissions.canAddClientAuthorization || client.managementPermissions.canDeleteClientAuthorization">
+              <h3>Client Authorizations</h3>
+              <span class="text-muted">The preferred method for authenticating clients is using an existing JWT to authenticate the client that is registered here.</span>
+              <button v-if="client.managementPermissions.canAddClientAuthorization" type="button" class="btn btn-success float-end" @click="openAddJwtBearerModal()" data-bs-toggle="modal" data-bs-target="#clientAddJwtBearerModal">
+                <i class="bi bi-plus"></i>
+              </button>
+              <div v-for="bearer in client.jwtBearer" :key="bearer.id">
+                <table class="table table-striped table-hover">
+                  <thead class="table-dark">
+                      <tr>
+                          <th scope="col">Field</th>
+                          <th scope="col">
+                            <div class="d-flex justify-content-between align-items-end">
+                              <div>Value</div>
+                              <button v-if="client.managementPermissions.canDeleteClientAuthorization" type="button" class="btn btn-danger float-end" @click="openDeauthorizeJwtBearerModal(bearer)" data-bs-toggle="modal" data-bs-target="#clientDeauthorizeJwtBearerModal" title="Delete Client JWT Bearer">
+                                <i class="bi bi-trash"></i>
+                              </button>
+                            </div>
+                          </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>JWKS URL</td>
+                          <td>{{ bearer.jwksUrl }}</td>
+                      </tr>
+                      <tr>
+                          <td>Issuer</td>
+                          <td>{{ bearer.iss }}</td>
+                      </tr>
+                      <tr>
+                          <td>Audience</td>
+                          <td>{{ bearer.aud }}</td>
+                      </tr>
+                      <tr>
+                          <td>Subject</td>
+                          <td>{{ bearer.sub }}</td>
+                      </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div v-if="client.jwtBearer.length == 0" class="text-center p-3">
+                <span class="fw-lighter">No client authorizations.</span>
+              </div>
+            </div>
+            <div v-if="client.managementPermissions.canAddClientSecret || client.managementPermissions.canDeleteClientSecret">
+              <h3>Client Secrets</h3>
+              <span class="text-muted">Client secrets are used to authenticate clients and are not preferred. Two client secrets can be active at a time to facilitate rotation.</span>
               <table class="table table-striped table-hover">
                 <thead class="table-dark">
                     <tr>
                         <th scope="col">Field</th>
-                        <th scope="col">
-                          <div class="d-flex justify-content-between align-items-end">
-                            <div>Value</div>
-                            <button v-if="client.managementPermissions.canDeleteClientAuthorization" type="button" class="btn btn-danger float-end" @click="openDeauthorizeJwtBearerModal(bearer)" data-bs-toggle="modal" data-bs-target="#clientDeauthorizeJwtBearerModal" title="Delete Client JWT Bearer">
-                              <i class="bi bi-trash"></i>
-                            </button>
-                          </div>
-                        </th>
+                        <th scope="col">Value</th>
+                        <th scope="col">Created</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>JWKS URL</td>
-                        <td>{{ bearer.jwksUrl }}</td>
+                        <td>Client Secret 1</td>
+                        <td>
+                          <span :class="{'me-2': true, 'badge': true, 'bg-primary': client.clientSecret1Set, 'bg-secondary': !client.clientSecret1Set}">
+                            {{ client.clientSecret1Set ? 'Set' : 'Not Set' }}
+                          </span>
+                        </td>
+                        <td>
+                          <DateTimeComponent v-if="client.clientSecret1Set" :date="client.clientSecret1Updated" />
+                        </td>
+                        <td>
+                            <div v-if="client.clientSecret1Set">
+                                <button v-if="client.managementPermissions.canDeleteClientSecret" type="button" class="btn btn-danger float-end" @click="openDeleteSecretModal('secret1')" data-bs-toggle="modal" data-bs-target="#deleteClientSecretModal" title="Delete Secret 1">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>
+                            <div v-else>
+                                <button v-if="client.managementPermissions.canAddClientSecret" type="button" class="btn btn-success float-end" @click="openSecretModal('secret1')" data-bs-toggle="modal" data-bs-target="#clientCreateSecretModal" title="Create Secret 1">
+                                    <i class="bi bi-plus"></i>
+                                </button>
+                            </div>
+                        </td>
                     </tr>
                     <tr>
-                        <td>Issuer</td>
-                        <td>{{ bearer.iss }}</td>
+                        <td>Client Secret 2</td>
+                        <td>
+                          <span :class="{'me-2': true, 'badge': true, 'bg-primary': client.clientSecret2Set, 'bg-secondary': !client.clientSecret2Set}">
+                            {{ client.clientSecret2Set ? 'Set' : 'Not Set' }}
+                          </span>
+                        </td>
+                        <td>
+                          <DateTimeComponent v-if="client.clientSecret2Set" :date="client.clientSecret2Updated" />
+                        </td>
+                        <td>
+                            <div v-if="client.clientSecret2Set">
+                                <button v-if="client.managementPermissions.canDeleteClientSecret" type="button" class="btn btn-danger float-end" @click="openDeleteSecretModal('secret2')" data-bs-toggle="modal" data-bs-target="#deleteClientSecretModal" title="Delete Secret 2">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>
+                            <div v-else>
+                                <button v-if="client.managementPermissions.canAddClientSecret" type="button" class="btn btn-success float-end" @click="openSecretModal('secret2')" data-bs-toggle="modal" data-bs-target="#clientCreateSecretModal" title="Create Secret 2">
+                                    <i class="bi bi-plus"></i>
+                                </button>
+                            </div>
+                        </td>
                     </tr>
-                    <tr>
-                        <td>Audience</td>
-                        <td>{{ bearer.aud }}</td>
-                    </tr>
-                    <tr>
-                        <td>Subject</td>
-                        <td>{{ bearer.sub }}</td>
-                    </tr>
-                </tbody>
+                  </tbody>
               </table>
             </div>
-            <div v-if="client.jwtBearer.length == 0" class="text-center p-3">
-              <span class="fw-lighter">No client authorizations.</span>
-            </div>
-
-            <button v-if="client.managementPermissions.canAddClientAuthorization" type="button" class="btn btn-success float-end" @click="openAddJwtBearerModal()" data-bs-toggle="modal" data-bs-target="#clientAddJwtBearerModal">
-              <i class="bi bi-plus"></i>
-            </button>
           </div>
         </div>
         <div class="row">
@@ -151,6 +200,7 @@
               <thead class="table-dark">
                 <tr>
                   <th scope="col">Client Id</th>
+                  <th scope="col">Scopes</th>
                   <th scope="col">Created</th>
                   <th scope="col" class="text-end">
                     <!-- Right now not allowing granting access to other resources on a client page, just granting access to other clients. -->
@@ -164,6 +214,9 @@
                 <!-- Loop through authorizationsAsSubject and display them in rows -->
                 <tr v-for="authorization in client.authorizationsAsSubject" :key="authorization.id">
                   <td><p class="text-break">{{ authorization.audience.clientId }}</p></td>
+                  <td>
+                    <span class="badge text-bg-dark" v-for="scope in authorization.authorizedScopes" :key="scope">{{ scope }}</span>
+                  </td>
                   <td><DateTimeComponent :date="authorization.authorizationCreated" /></td>
                   <td class="text-end">
                     <div class="btn-group" role="group">
@@ -193,6 +246,7 @@
               <thead class="table-dark">
                 <tr>
                   <th scope="col">Client Id</th>
+                  <th scope="col">Scopes</th>
                   <th scope="col">Created</th>
                   <th scope="col" class="text-end">
                     <button v-if="client.managementPermissions.canAddAuthorization" type="button" class="btn btn-success float-end" @click="resetAuthorize()" data-bs-toggle="modal" data-bs-target="#clientAuthorizeModal">
@@ -205,6 +259,9 @@
                 <!-- Loop through authorizationsAsAudience and display them in rows -->
                 <tr v-for="authorization in client.authorizationsAsAudience" :key="authorization.id">
                   <td><p class="text-break">{{ authorization.subject.clientId }}</p></td>
+                  <td>
+                    <span class="badge text-bg-dark" v-for="scope in authorization.authorizedScopes" :key="scope">{{ scope }}</span>
+                  </td>
                   <td><DateTimeComponent :date="authorization.authorizationCreated" /></td>
                   <td class="text-end">
                     <div class="btn-group" role="group">
@@ -242,6 +299,7 @@ import ClientAuthorizeModal from './ClientAuthorizeModal.vue';
 import ClientDeauthorizeModal from './ClientDeauthorizeModal.vue';
 import ClientAddJwtBearerModal from './ClientAddJwtBearerModal';
 import clientDeauthorizeJwtBearerModal from './ClientDeauthorizeJwtBearerModal.vue';
+import ClientAddAvailableScopeModal from './ClientAddAvailableScopeModal.vue';
 
 const GET_CLIENT = gql`
   query Client($clientId: ID!) { 
@@ -254,9 +312,14 @@ const GET_CLIENT = gql`
       clientSecret1Updated
       clientSecret2Set
       clientSecret2Updated
+      availableScopes {
+        scope
+        description
+      }
       authorizationsAsSubject {
         id
         authorizationCreated
+        authorizedScopes
         audience {
           clientId
         }
@@ -264,6 +327,7 @@ const GET_CLIENT = gql`
       authorizationsAsAudience {
         id
         authorizationCreated
+        authorizedScopes
         subject {
           clientId
         }
@@ -298,6 +362,7 @@ export default {
       ClientDeauthorizeModal,
       ClientAddJwtBearerModal,
       clientDeauthorizeJwtBearerModal,
+      ClientAddAvailableScopeModal,
   },
   data() {
     return {
