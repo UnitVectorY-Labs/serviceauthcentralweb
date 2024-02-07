10c88ff7b4c883a1dae704b88c3ceaaeef81e891
Jared Hatfield <jjhatf02@gmail.com>
2024-02-06 19:50:11 -0500

    Broke out ClientAuthorizedAsSubjectComponent

--- src/components/ClientDetailsPage.vue
+++ src/components/ClientDetailsPage.vue
@@ -24,49 +24,7 @@
         <div class="row">
 
           <div class="col-lg-6 col-sm-12">
-            <!-- Display authorizations for subjects if client is not null -->
-            <h3>Authorized as Subject</h3>
-            <span class="text-muted">The "<span class="text-primary fw-bold">{{ client.clientId }}</span>" client is authorized to access the following clients.</span>
-            <table class="table table-striped table-hover mt-2">
-              <!-- Table headers here -->
-              <thead class="table-dark">
-                <tr>
-                  <th scope="col">Client Id</th>
-                  <th scope="col">Scopes</th>
-                  <th scope="col">Created</th>
-                  <th scope="col" class="text-end">
-                    <!-- Right now not allowing granting access to other resources on a client page, just granting access to other clients. -->
-                    <button type="button" class="btn btn-success float-end invisible" disabled="disabled" >
-                      <i class="bi bi-plus"></i>
-                    </button>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <!-- Loop through authorizationsAsSubject and display them in rows -->
-                <tr v-for="authorization in client.authorizationsAsSubject" :key="authorization.id">
-                  <td><p class="text-break">{{ authorization.audience.clientId }}</p></td>
-                  <td>
-                    <span class="badge text-bg-dark" v-for="scope in authorization.authorizedScopes" :key="scope">{{ scope }}</span>
-                  </td>
-                  <td><DateTimeComponent :date="authorization.authorizationCreated" /></td>
-                  <td class="text-end">
-                    <div class="btn-group" role="group">
-                      <!-- Right now not allowing deauthorize either -->
-                      <button type="button" class="btn btn-danger float-end invisible" disabled="disabled">
-                        <i class="bi bi-trash"></i>
-                      </button>
-                      <router-link :to="'/clients/' + encodeURIComponent(authorization.audience.clientId)" class="btn btn-primary">
-                        <i class="bi bi-arrow-right"></i>
-                      </router-link>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <div v-if="client.authorizationsAsSubject.length == 0" class="text-center p-3">
-              <span class="fw-lighter">No authorizations.</span>
-            </div>
+            <ClientAuthorizedAsSubjectComponent :client="client" />
           </div>
 
           <div class="col-lg-6 col-sm-12" v-if="client.managementPermissions.canAddAuthorization || client.managementPermissions.canDeleteAuthorization" >
@@ -129,6 +87,7 @@ import ClientInfoComponent from './ClientInfoComponent.vue';
 import ClientAvailableScopesComponent from './ClientAvailableScopesComponent.vue';
 import ClientJwtBearerComponent from './ClientJwtBearerComponent.vue';
 import ClientSecretsComponent from './ClientSecretsComponent.vue';
+import ClientAuthorizedAsSubjectComponent from './ClientAuthorizedAsSubjectComponent.vue';
 
 import ClientAuthorizeModal from './ClientAuthorizeModal.vue';
 import ClientDeauthorizeModal from './ClientDeauthorizeModal.vue';
@@ -192,6 +151,7 @@ export default {
       ClientAvailableScopesComponent,
       ClientJwtBearerComponent,
       ClientSecretsComponent,
+      ClientAuthorizedAsSubjectComponent,
       ClientAuthorizeModal,
       ClientDeauthorizeModal,
   },
